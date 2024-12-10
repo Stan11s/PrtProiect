bed6187d234e562f4479d25d3a1d247c2d84f779
Staniislaw <stas.vip.07@mail.ru>
2024-12-10 10:07:08 +0200

    Adăugat proiectul C#

---
+++ source/repos/PrtProiect/PrtProiect/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace PrtProiect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panelDragDrop = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.panelDragDrop.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightBlue;
+            this.button1.Location = new System.Drawing.Point(406, 482);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(540, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Read PDF Files";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.richTextBox1.Location = new System.Drawing.Point(406, 36);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(540, 430);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(75, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.InfoText;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(6, 24);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(526, 424);
+            this.listBox1.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panelDragDrop);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(20, 20);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(367, 471);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input Settings";
+            // 
+            // panelDragDrop
+            // 
+            this.panelDragDrop.AllowDrop = true;
+            this.panelDragDrop.BackColor = System.Drawing.SystemColors.Info;
+            this.panelDragDrop.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelDragDrop.Controls.Add(this.label4);
+            this.panelDragDrop.Location = new System.Drawing.Point(100, 68);
+            this.panelDragDrop.Name = "panelDragDrop";
+            this.panelDragDrop.Size = new System.Drawing.Size(147, 78);
+            this.panelDragDrop.TabIndex = 7;
+            this.panelDragDrop.DragDrop += new System.Windows.Forms.DragEventHandler(this.panelDragDrop_DragDrop);
+            this.panelDragDrop.DragEnter += new System.Windows.Forms.DragEventHandler(this.panelDragDrop_DragEnter);
+            this.panelDragDrop.Paint += new System.Windows.Forms.PaintEventHandler(this.panelDragDrop_Paint);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(139, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Drag  and drop fisier pdf aici";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(146, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Path Input:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "PDF Files|*.pdf";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listBox1);
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox2.Location = new System.Drawing.Point(968, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(538, 500);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Output Settings";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(1509, 524);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "PDF Reader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panelDragDrop.ResumeLayout(false);
+            this.panelDragDrop.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Panel panelDragDrop;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
---
+++ source/repos/PrtProiect/PrtProiect/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Parser;
+using iText.Kernel.Pdf.Canvas.Parser.Listener;
+
+namespace PrtProiect
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string pdfPathFromDragDrop = "";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(pdfPathFromDragDrop))
+            {
+                pdfPath = pdfPathFromDragDrop;
+            }
+            try
+            {
+                
+
+                if (!System.IO.File.Exists(pdfPath))
+                {
+                    MessageBox.Show("Fișierul PDF nu a fost găsit.");
+                    return;
+                }
+
+                PdfDocument pdfDocument = new PdfDocument(new PdfReader(pdfPath));
+                StringBuilder extractedText = new StringBuilder();
+
+                for (int i = 1; i <= pdfDocument.GetNumberOfPages(); i++)
+                {
+                    var page = pdfDocument.GetPage(i);
+                    var strategy = new SimpleTextExtractionStrategy();
+                    string text = PdfTextExtractor.GetTextFromPage(page, strategy);
+
+                    extractedText.AppendLine($"Textul de pe pagina {i}:");
+                    extractedText.AppendLine(text);
+                    extractedText.AppendLine();
+                }
+
+                
+
+                string textFromPdf = extractedText.ToString();
+                textFromPdf = textFromPdf.Replace("\n", " ").Replace("\r", " ");
+                richTextBox1.Text = extractedText.ToString();
+
+
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Căutăm informații din textul PDF...");
+
+                string dateTimePattern = @"(\d{2}-\d{2}-\d{4} \d{2}:\d{2}:\d{2})";
+                Match dateTimeMatch = Regex.Match(textFromPdf, dateTimePattern);
+                if (dateTimeMatch.Success)
+                {
+                    string dateTime = dateTimeMatch.Groups[1].Value;
+                    listBox1.Items.Add($"Data și ora: {dateTime}");
+                }
+
+                // Căutare Curier
+                string courierPattern = @"DATA ORA\s+(([A-Z]+\s+)*[A-Z]+)";
+                Match courierMatch = Regex.Match(textFromPdf, courierPattern);
+                if (courierMatch.Success)
+                {
+                    string courier = courierMatch.Groups[1].Value;
+                    listBox1.Items.Add($"Curier: {courier}");
+                }
+
+                /*
+                 * Regexul consta din sa inceapa cu Destinatar sau recipient apoi urmata de numele lui si pentru 4 grup avem un lookahead sa stim unde sa ne oprim 
+                 * folosin (.*?) indetificam cea mai mica potrivire 
+                 */
+                string recipientPattern = @"(DESTINATAR|recipient)\s+([a-zA-Z]+\s+[a-zA-Z]+)\s*(.*?)(?=\s*\d{10}\s)";
+
+                Match recipientMatch = Regex.Match(textFromPdf, recipientPattern);
+                if (recipientMatch.Success)
+                {
+                    string name = recipientMatch.Groups[2].Value; 
+                    string address = recipientMatch.Groups[3].Value; 
+
+                    listBox1.Items.Add($"Destinatar: {name}");
+                    listBox1.Items.Add($"Adresă: {address}");
+                }
+                else
+                {
+                    listBox1.Items.Add("Destinatarul nu a fost găsit.");
+                }
+
+
+
+                string emailPattern = @"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}";
+                Match emailMatch = Regex.Match(textFromPdf, emailPattern);
+                if (emailMatch.Success)
+                {
+                    listBox1.Items.Add($"Email-ul: {emailMatch.Value}");
+                }
+
+                string phonePattern = @"\s+[0-9]{10}\s+"; 
+                Match phoneMatch = Regex.Match(textFromPdf, phonePattern);
+                if (phoneMatch.Success)
+                {
+                    listBox1.Items.Add($"Numărul de telefon: {phoneMatch.Value}");
+                }
+                string optionsPattern = @"(Optiuni)\s+([A-Za-z\s]+)(?=\s*(Doc))";
+                Match optionsMatch = Regex.Match(textFromPdf, optionsPattern);
+                if (phoneMatch.Success)
+                {
+                    string options = optionsMatch.Groups[2].Value;
+                    listBox1.Items.Add($"optiuni pentru livrare: {options}");
+                }
+                string observationsPattern = @"OBSERVATII\s+Comanda:\s*(\d+#\s*\d+);\s*Produse:\s*([^\;]+);\s*AWB:\s*([^\;]*)\s*";
+                Match observationsMatch = Regex.Match(textFromPdf, observationsPattern);
+                if (observationsMatch.Success)
+                {
+                    string comanda = observationsMatch.Groups[1].Value; // Comanda
+                    string produse = observationsMatch.Groups[2].Value; // Produse
+                    string awb = observationsMatch.Groups[3].Value; // AWB
+
+                    listBox1.Items.Add($"Comanda: {comanda}");
+                    listBox1.Items.Add($"Produse: {produse}");
+                    listBox1.Items.Add($"AWB: {awb}");
+                }
+                else
+                {
+                    listBox1.Items.Add("Nu au fost găsite observații.");
+                }
+
+                string contentPattern = @"CONTINUT\s+(.+?)(?=\s+\d{15,})"; //pattern pentru cuvintele care incep cu CONTINUT si folosim  lookahead pentru a ne asigura că ne oprim înainte de primul număr de cel puțin 15 caractere 
+                Match contentMatch = Regex.Match(textFromPdf, contentPattern);
+                if (contentMatch.Success)
+                {
+                    string content = contentMatch.Groups[1].Value;
+                    listBox1.Items.Add($"Conținut: {content}");
+                }
+                else
+                {
+                    listBox1.Items.Add("Nu au fost găsite informații despre conținut.");
+                }
+
+                if (listBox1.Items.Count == 0)
+                {
+                    listBox1.Items.Add("Nu au fost găsite informații relevante.");
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Eroare la procesarea PDF-ului: {ex.Message}");
+            }
+        }
+        private void panelDragDrop_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy; 
+            }
+        }
+
+        private void panelDragDrop_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            if (files.Length > 0)
+            {
+                string filePath = files[0]; 
+                textBox1.Text = filePath;   
+                pdfPathFromDragDrop = files[0];
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        string pdfPath;
+        private void panelDragDrop_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
